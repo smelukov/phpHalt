bd541fe98044e186aa927b4e750b87b9456ec5db
Sergey Melyukov <s.melukov@gmail.com>
2014-08-12 03:04:17 +0400

    Initial commit
    For my article - http://smelukov.com/php-halt/

---
+++ halt-test.php
@@ -0,0 +1,43 @@
+<?@unlink(__DIR__.'/log.txt');
+
+ob_implicit_flush(false);
+function toLog($what)
+{
+    file_put_contents(__DIR__.'/log.txt', $what.PHP_EOL, FILE_APPEND);
+}
+
+register_shutdown_function(function()
+{
+    toLog('halted');
+});
+ob_start();
+echo 'dummy';
+
+ob_start();
+echo 'dummy2';
+
+ob_start();
+
+for($i=2; $i<=20; $i++)
+{
+    $buffers = array();
+    echo ($i*$i).'<br>';
+    while(ob_get_level())
+        array_unshift($buffers, ob_get_clean());
+    echo ' ';
+    flush();
+    if(connection_aborted())
+        exit;
+    else
+        foreach($buffers as $content)
+        {
+            ob_start();
+            echo $content;
+        }
+    sleep(1);
+}
+
+while(ob_get_level()>1)
+    ob_end_flush();
+$data = ob_get_clean();
+echo "Результат мега-вычеслений:<br>".$data;
